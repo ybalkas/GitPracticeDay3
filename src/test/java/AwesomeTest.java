b14b8f0c607c7d4bd3150d7097b12f35f88463f6
Yusuf Balkas <ybalkas@gmail.com>
2020-10-06 21:19:58 +0100

    I added new file

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -1,3 +1,9 @@
 public class AwesomeTest {
+    public static void main(String[] args) {
+        System.out.println("hello how");
+        System.out.println("hello how are you");
+        System.out.println("ne var ne yok");
+        System.out.println("efrf");
+    }
 
 }
